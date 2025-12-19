c84494d3019ca8f75ee948fedfd1d352de1e3b47
Garber1329 <91221454+Garber1329@users.noreply.github.com>
2025-12-19 16:33:34 +0200

    fix render

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,16 +1,16 @@
 import ProductItem from "../Components/ProductItem"
 
 const ProductList = ({ title, products = [] }) => (
-    
+
   <>
-   { console.log("ProductList render:", title, products)}
+    {console.log("ProductList render:", title, products)}
     <h2>{title}</h2>
     <ul>
-   {products.map(p => (
-  <li key={p.id}>
-    <ProductItem product={p} />
-  </li>
-))}
+      {Array.isArray(products) && products.map(p => (
+        <li key={p._id || p.id}>
+          <ProductItem product={p} />
+        </li>
+      ))}
     </ul>
   </>
 )
